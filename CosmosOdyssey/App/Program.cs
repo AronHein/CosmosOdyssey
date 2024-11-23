2256cf29da1a3735723b94be10a8b2e1b8a8b25a
AronHein <heinaron@gmail.com>
2024-11-23 12:09:14 +0200

    Setup Models project and added initial api call

---
+++ CosmosOdyssey/App/Program.cs
@@ -0,0 +1,6 @@
+﻿using var client = new HttpClient();
+const string apiUrl = "https://cosmos-odyssey.azurewebsites.net/api/v1.0/TravelPrices";
+
+var response = await client.GetAsync(apiUrl);
+var content = await response.Content.ReadAsStringAsync();
+Console.WriteLine(content);
