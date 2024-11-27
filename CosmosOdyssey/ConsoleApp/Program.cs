33d68e91d2190fd3b49c31a77cc6d2dfc2166d25
AronHein <heinaron@gmail.com>
2024-11-27 12:36:26 +0200

    added fetching for json

--- CosmosOdyssey/ConsoleApp/Program.cs
+++ CosmosOdyssey/ConsoleApp/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using DAL;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddHttpClient();
+serviceCollection.AddTransient<PricelistService>();
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var pricelistService = serviceProvider.GetService<PricelistService>();
+
+var json = await pricelistService.FetchPricelistJson();
+Console.WriteLine(json);
---
+++ CosmosOdyssey/DAL/PricelistService.cs
@@ -0,0 +1,27 @@
+﻿namespace DAL;
+
+public class PricelistService
+{
+    private readonly HttpClient _client;
+    private const string ApiUrl = "https://cosmos-odyssey.azurewebsites.net/api/v1.0/TravelPrices";
+
+    public PricelistService(HttpClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<string> FetchPricelistJson()
+    {
+        try
+        {
+            var response = await _client.GetAsync(ApiUrl);
+            var content = await response.Content.ReadAsStringAsync();
+            return content;
+
+        }
+        catch (Exception e)
+        {
+            throw new Exception("Error occurred while trying to fetch data from the API: " + e);
+        }
+    }
+}
