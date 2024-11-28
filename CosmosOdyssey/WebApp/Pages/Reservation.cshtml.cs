f76ebb13b08b3c6d0f5d871252b21424c57d2e80
AronHein <heinaron@gmail.com>
2024-11-28 17:04:15 +0200

    added Reservation page that shows all routes

---
+++ CosmosOdyssey/WebApp/Pages/Reservation.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models;
+using DAL;
+
+namespace WebApp.Pages
+{
+    public class ReservationModel : PageModel
+    {
+        private readonly PricelistService _pricelistService;
+
+        public ReservationModel(PricelistService pricelistService)
+        {
+            _pricelistService = pricelistService;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public string From { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string To { get; set; }
+        
+        [BindProperty(SupportsGet = true)]
+        public Pricelist Pricelist { get; set; }
+        
+        [BindProperty]
+        public List<List<Leg>> Routes { get; set; }
+
+        public async Task OnGet()
+        {
+            if (!string.IsNullOrEmpty(From) && !string.IsNullOrEmpty(To))
+            { 
+                Pricelist = await _pricelistService.GetLatestPricelist();
+                Routes = _pricelistService.FindAllRoutes(Pricelist, From, To);
+            }
+        }
+    }
+}
