95c33b822dab46c2afcdd8fccb12bd4a60e18178
AronHein <heinaron@gmail.com>
2024-11-30 13:42:08 +0200

    Refactored all reservations formatting functions. added formating for total travel time

--- CosmosOdyssey/WebApp/Pages/AllReservations.cshtml.cs
+++ CosmosOdyssey/WebApp/Pages/AllReservations.cshtml.cs
@@ -19,4 +19,40 @@ public class AllReservationsModel : PageModel
     {
         Reservations = await _reservationService.GetReservations();
     }
+    
+    public static string FormatTimeSpan(TimeSpan timeSpan)
+    {
+        string formattedTime = "";
+
+        if (timeSpan.Days > 0)
+            formattedTime += $"{timeSpan.Days} day{(timeSpan.Days > 1 ? "s" : "")}";
+
+        if (timeSpan.Hours > 0)
+        {
+            if (formattedTime.Length > 0)
+                formattedTime += ", ";
+
+            formattedTime += $"{timeSpan.Hours} hour{(timeSpan.Hours > 1 ? "s" : "")}";
+        }
+
+        if (timeSpan.Minutes > 0)
+        {
+            if (formattedTime.Length > 0)
+                formattedTime += ", ";
+
+            formattedTime += $"{timeSpan.Minutes} minute{(timeSpan.Minutes > 1 ? "s" : "")}";
+        }
+
+        return formattedTime;
+    }
+    
+    public static string FormatRoute(List<RouteInfo> routes)
+    {
+        var fullRoute = routes.First().From.Name;
+        foreach (var routeInfo in routes)
+        {
+            fullRoute += " -> " + routeInfo.To.Name;
+        }
+        return fullRoute;
+    }
 }
